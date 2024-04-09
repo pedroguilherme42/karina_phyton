d267525138fc63b688404699e87a8d9ce5c3d16d
servanthyofgod <154385583+pedroguilherme42@users.noreply.github.com>
2024-04-08 21:26:13 -0300

    Add files via upload

---
+++ 1_.py
@@ -0,0 +1,5 @@
+num = int(input("Digite o valor de um número"))
+if num <= 9:
+           print("Valor Correto")
+else:
+           print("Valor incorreto")
---
+++ 2.py
@@ -0,0 +1,11 @@
+turno = (input("Qual seu turno de trabalho?"))
+Horas = float(input("Horas Trabalhadas no mês"))
+if turno.upper == (n):
+    valor_hora = 45.00
+              
+              
+else:
+    valor_hora = 37.50
+
+salario = Horas*valor_hora
+print("O valor do sálario é R$",salario)
